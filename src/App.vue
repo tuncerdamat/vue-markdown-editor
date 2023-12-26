b6595795d456a1183ec161d526ab0d3a09accce8
tuncerdamat <tuncer.damat@gmail.com>
2023-12-26 16:00:24 +0100

    Vuejs.org - Markdown editor - Added babel/plugin-transform-private-methods to babel config to fix compilation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<!-- script setup is a compile-time syntactic sugar for using Composition API inside Single-File Components (SFCs).
+     It is the recommended syntax if you are using both SFCs and Composition API.
+-->
+<script setup>
+import { marked } from 'marked'
+import { debounce } from 'lodash-es'
+import { ref, computed } from 'vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const input = ref('# hello')
+const output = computed(() => marked(input.value))
+const update = debounce((e) => {
+  input.value = e.target.value
+}, 100)
 </script>
 
+<template>
+  <div class="editor">
+    <textarea class="input" 
+              :value="input"
+              @input="update"
+    >
+    </textarea>
+    <div class="output" v-html="output"></div>
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+}
+
+.editor {
+  height: 100vh;
+  display: flex;
+}
+
+.input,
+.output {
+  overflow: auto;
+  width: 50%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+.input {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
 }
 </style>
